88fc7e7b0072300b5b34f58e1e56028e8a42f270
PaTonno <riccardopatuano@gmail.com>
2022-05-10 20:08:42 +0200

    0.2
    0.2 version with basic, non-functional UI with HR gauge. version 0.1 lost to overwriting. Some demo-code still present.

---
+++ hr_beta/android/app/src/main/kotlin/com/example/hr_beta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hr_beta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hr_beta/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'Welcome to Flutter',
+
+      home: Scaffold(
+        appBar: AppBar(
+
+          title: const Text('Welcome to Flutter'),
+
+        ),
+        body: Center(
+
+          child: Container(
+            
+            child: SfRadialGauge(
+
+              title: const GaugeTitle(
+                
+                text: 'Heart Rate',
+
+                textStyle: TextStyle(
+
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                  fontFamily: 'Times'
+
+                ),
+                
+                alignment: GaugeAlignment.center
+              
+              ),
+
+              axes: <RadialAxis>[RadialAxis(
+
+                minimum: 50, maximum: 200, labelOffset: 20, 
+                axisLineStyle: const AxisLineStyle(thickness: 30), showTicks: false,
+
+                ranges: <GaugeRange>[
+
+                  GaugeRange(startValue: 50, endValue: 100, color: Colors.grey, startWidth: 30, endWidth: 30),
+                  GaugeRange(startValue: 100, endValue: 200, startWidth: 30, endWidth: 30,
+                    gradient: const SweepGradient(
+                      colors: <Color>[Colors.green,Colors.yellow,Colors.red],
+                      stops: <double>[0.0,0.5,1]                    
+                    )
+                  )
+
+                ],
+
+                pointers: const <GaugePointer>[
+                  NeedlePointer(
+
+                    value: 140,
+                    enableAnimation: true,
+                    needleStartWidth: 0, needleEndWidth: 5, needleColor: Colors.black,
+                    knobStyle: KnobStyle(
+
+                      color: Colors.white, borderColor: Colors.black, 
+                      knobRadius: 0.04, borderWidth: 0.02
+
+                    ),
+                    tailStyle: TailStyle(
+
+                      color: Colors.black, width: 5, length: 0
+
+                    )
+
+                  ),
+
+                ],
+              )
+              ]
+            )
+          )
+         )
+      ),
+    );
+  }
+}
