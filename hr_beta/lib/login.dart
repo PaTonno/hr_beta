b8a9d1931cc839b5c076bcb56f7899d9610c8d55
PaTonno <riccardopatuano@gmail.com>
2022-05-11 14:02:16 +0200

    0.3.1
    front-end login page nearly finished, still non-functional and non-interactive with main app. Included assets for login page.

--- hr_beta/lib/login.dart
+++ hr_beta/lib/login.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+
+//MISSING ON-PRESSED FUNCTIONS FOR BUTTONS
+
 class LoginPage extends StatefulWidget {
 
   const LoginPage({Key? key}) : super(key : key);
@@ -10,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,30 +28,51 @@ class _LoginPageState extends State<LoginPage> {
             Column(
               children: <Widget>[
 
-                Image.asset('assets/diamond.png'),
+                Image.asset('assets/menudino.png'),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                const Text('RUN FROM RAPTORS'),
                 
               ],
             ),
             
             const SizedBox(height: 120.0),
-            const TextField(
-
-              decoration: InputDecoration(
+             TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Username',
-
               ),
             ),
             const SizedBox(height: 12),
-            const TextField(
-              decoration: InputDecoration(
+             TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Password',
               ),
             obscureText: true,
             ),
+          
+          OverflowBar(
+            alignment: MainAxisAlignment.end,
+            children: <Widget>[
+
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: (){
+
+                },
+              ),
+
+              ElevatedButton(
+                child: const Text('NEXT'),
+                onPressed: (){
+
+                },
+              )
+
+            ]
+          )
             // TODO: Remove filled: true values (103)
             // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
